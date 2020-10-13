9e63873d6c96ddf984c1604ab9a62b5e124b9498
awakexzxy <awakexzxy@163.com>
2020-10-13 17:11:10 +0800

    Change loan router to lend.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -30,7 +30,7 @@ import Swap from './Swap'
 import { OpenClaimAddressModalAndRedirectToSwap, RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
 import Vote from './Vote'
 import VotePage from './Vote/VotePage'
-import Loan from './Loan'
+import Lend from './Lend'
 
 const AppWrapper = styled.div`
   display: flex;
@@ -90,7 +90,7 @@ export default function App() {
           <TopLevelModals />
           <Web3ReactManager>
             <Switch>
-              <Route exact strict path="/loan" component={Loan} />
+              <Route exact strict path="/lend" component={Lend} />
               <Route exact strict path="/swap" component={Swap} />
               <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
               <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
