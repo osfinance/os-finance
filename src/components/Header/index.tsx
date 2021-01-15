37543beece684af956d37f41df7167b8c17f53d6
QIUCHENGHUI <243852877@qq.com>
2021-01-15 17:40:04 +0800

    Update lending header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { ChainId, TokenAmount } from '@uniswap/sdk'
+import { ChainId } from '@uniswap/sdk'
 import React, { useState } from 'react'
 import { Text } from 'rebass'
 import { NavLink } from 'react-router-dom'
@@ -11,9 +11,8 @@ import Logo from '../../assets/svg/logo.svg'
 import LogoDark from '../../assets/svg/logo_white.svg'
 import { useActiveWeb3React } from '../../hooks'
 import { useDarkModeManager } from '../../state/user/hooks'
-import { useETHBalances, useAggregateUniBalance } from '../../state/wallet/hooks'
+import { useETHBalances } from '../../state/wallet/hooks'
 import { CardNoise } from '../earn/styled'
-import { CountUp } from 'use-count-up'
 import { TYPE, ExternalLink } from '../../theme'
 
 import { YellowCard } from '../Card'
@@ -29,7 +28,6 @@ import { useUserHasSubmittedClaim } from '../../state/transactions/hooks'
 import { Dots } from '../swap/styleds'
 import Modal from '../Modal'
 import UniBalanceContent from './UniBalanceContent'
-import usePrevious from '../../hooks/usePrevious'
 
 const HeaderFrame = styled.div`
   display: grid;
@@ -225,7 +223,7 @@ const StyledNavLink = styled(NavLink).attrs({
 
 const StyledExternalLink = styled(ExternalLink).attrs({
   activeClassName
-})<{ isActive?: boolean }>`
+})<{ isActive?: boolean; smallHide?: boolean }>`
   ${({ theme }) => theme.flexRowNoWrap}
   align-items: left;
   border-radius: 3rem;
@@ -249,9 +247,9 @@ const StyledExternalLink = styled(ExternalLink).attrs({
     color: ${({ theme }) => darken(0.1, theme.text1)};
   }
 
-  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
-      display: none;
-`}
+  ${({ theme, smallHide }) => theme.mediaWidth.upToExtraSmall`
+      display: ${smallHide ? 'none' : 'block'};
+  `}
 `
 
 export const StyledMenuButton = styled.button`
@@ -304,13 +302,13 @@ export default function Header() {
 
   const { claimTxn } = useUserHasSubmittedClaim(account ?? undefined)
 
-  const aggregateBalance: TokenAmount | undefined = useAggregateUniBalance()
+  // const aggregateBalance: TokenAmount | undefined = useAggregateUniBalance()
 
   const [showUniBalanceModal, setShowUniBalanceModal] = useState(false)
   const showClaimPopup = useShowClaimPopup()
 
-  const countUpValue = aggregateBalance?.toFixed(0) ?? '0'
-  const countUpValuePrevious = usePrevious(countUpValue) ?? '0'
+  // const countUpValue = aggregateBalance?.toFixed(0) ?? '0'
+  // const countUpValuePrevious = usePrevious(countUpValue) ?? '0'
 
   return (
     <HeaderFrame>
@@ -325,6 +323,7 @@ export default function Header() {
           </UniIcon>
         </Title>
         <HeaderLinks>
+          {/* 
           <StyledNavLink id={`swap-nav-link`} to={'/swap'}>
             {t('swap')}
           </StyledNavLink>
@@ -341,6 +340,20 @@ export default function Header() {
           >
             {t('pool')}
           </StyledNavLink>
+          */}
+          <StyledNavLink id={`lending-nav-link`} to={'/lending'}>
+            {t('lending')}
+          </StyledNavLink>
+          <StyledExternalLink id={`twitter-nav-link`} href={'https://twitter.com/deerfi_com'} smallHide={false}>
+            Twitter <span style={{ fontSize: '11px' }}>↗</span>
+          </StyledExternalLink>
+          <StyledExternalLink id={`discord-nav-link`} href={'https://discord.gg/SHdfFgX'} smallHide={false}>
+            Discord <span style={{ fontSize: '11px' }}>↗</span>
+          </StyledExternalLink>
+          <StyledExternalLink id={`discord-nav-link`} href={'https://defipulse.com/defi-list'} smallHide={true}>
+            DeFi Pulse <span style={{ fontSize: '11px' }}>↗</span>
+          </StyledExternalLink>
+          {/* 
           <StyledNavLink id={`stake-nav-link`} to={'/uni'}>
             UNI
           </StyledNavLink>
@@ -350,6 +363,7 @@ export default function Header() {
           <StyledExternalLink id={`stake-nav-link`} href={'https://uniswap.info'}>
             Charts <span style={{ fontSize: '11px' }}>↗</span>
           </StyledExternalLink>
+          */}
         </HeaderLinks>
       </HeaderRow>
       <HeaderControls>
@@ -369,6 +383,7 @@ export default function Header() {
               <CardNoise />
             </UNIWrapper>
           )}
+          {/* 
           {!availableClaim && aggregateBalance && (
             <UNIWrapper onClick={() => setShowUniBalanceModal(true)}>
               <UNIAmount active={!!account && !availableClaim} style={{ pointerEvents: 'auto' }}>
@@ -395,6 +410,7 @@ export default function Header() {
               <CardNoise />
             </UNIWrapper>
           )}
+          */}
           <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
             {account && userEthBalance ? (
               <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
