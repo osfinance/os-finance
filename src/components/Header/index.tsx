248d0ab85cf111a6df63175d829e5c5cdbf10602
QIUCHENGHUI <243852877@qq.com>
2021-01-19 01:50:45 +0800

    Fix header logo router

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -298,7 +298,6 @@ const NETWORK_LABELS: { [chainId in ChainId]?: string } = {
 
 export default function Header() {
   const { account, chainId } = useActiveWeb3React()
-  const { t } = useTranslation()
 
   const location = useLocation()
   const page = location.pathname.split('/')[1]
@@ -314,7 +313,7 @@ export default function Header() {
   return (
     <HeaderFrame>
       <HeaderRow>
-        <Title href=".">
+        <Title href={`/#/${page}/`}>
           {PageFields.HOME === page && (
             <UniIcon>
               <EmojiIcon role="img">{'😋'}</EmojiIcon>
@@ -338,7 +337,7 @@ export default function Header() {
         </Title>
         <HeaderLinks>
           <StyledNavLink id={`swap-nav-link`} to={'/home'}>
-            {t('home')}
+            OS Finance
           </StyledNavLink>
           {PageFields.UNISWAP === page && <UniswapHeaderLinks />}
           {PageFields.SUSHISWAP === page && <SushiswapHeaderLinks />}
